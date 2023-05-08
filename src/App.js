753449277d8e6b8b4ad297257ca6dad3a37d9a60
Rajan Chaudhary <60035342+rajann44@users.noreply.github.com>
2023-05-09 00:15:42 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import CreatePost from "./components/CreatePost";
+import Navbar from "./components/Navbar";
 import PostsList from "./components/PostsList";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<PostsList></PostsList>}></Route>
-      <Route path="/create" element={<CreatePost></CreatePost>}></Route>
-    </Routes>
+    <>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<PostsList></PostsList>}></Route>
+        <Route path="/create" element={<CreatePost></CreatePost>}></Route>
+      </Routes>
+    </>
   );
 }
 
--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -17,7 +17,9 @@ const PostsList = () => {
 
   return (
     <section class="w-screen py-5">
-      <h1 class="mb-2 text-center font-sans text-5xl font-bold">Our Blog</h1>
+      <h1 class="mb-2 text-center font-sans text-5xl font-bold my-14">
+        Our Blog
+      </h1>
       <div class="mx-auto grid max-w-screen-lg grid-cols-1 gap-5 p-5 sm:grid-cols-2 md:grid-cols-3 lg:gap-10">
         {postDetails.map((post, index) => (
           <article
